432af912011bc56a72852137ce24aa04c9269832
lsd-gb <57203364+lsd-gb@users.noreply.github.com>
2019-12-26 20:59:47 +0800

    12.26.20.59 完善entity

---
+++ project/src/main/java/entity/Custom.java
@@ -0,0 +1,35 @@
+package entity;
+
+import java.io.Serializable;
+
+public class Custom implements Serializable {
+	private Integer id;
+	private String name;
+	private Integer userid;
+	private String username;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getUserid() {
+		return userid;
+	}
+	public void setUserid(Integer userid) {
+		this.userid = userid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+}
---
+++ project/src/main/java/entity/User_role.java
@@ -0,0 +1,42 @@
+package entity;
+
+import java.io.Serializable;
+
+public class User_role implements Serializable {
+	private Integer id;
+	private Integer userid;
+	private String username;
+	private Integer roleid;
+	private String rolename;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getUserid() {
+		return userid;
+	}
+	public void setUserid(Integer userid) {
+		this.userid = userid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Integer getRoleid() {
+		return roleid;
+	}
+	public void setRoleid(Integer roleid) {
+		this.roleid = roleid;
+	}
+	public String getRolename() {
+		return rolename;
+	}
+	public void setRolename(String rolename) {
+		this.rolename = rolename;
+	}
+	
+}
